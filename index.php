64b3016b41392eb1fe82a4b12d736b81e8a2e295
arikurniawans <ari.curnia.one@gmail.com>
2019-05-02 16:48:37 +0700

    Backup Web Inovasi PPIK 2019

---
+++ index.php
@@ -0,0 +1,259 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Inovasi UBL 2019</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://adminlte.io/themes/AdminLTE/bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="https://adminlte.io/themes/AdminLTE/dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="https://adminlte.io/themes/AdminLTE/dist/css/skins/_all-skins.min.css">
+  <link href="https://www.cssscript.com/demo/pure-css-checkbox-radio-button-replacement-bootstrap-icheck/icheck-bootstrap.css" rel="stylesheet" type="text/css">
+
+  <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+  <!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+  <![endif]-->
+
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+  
+  </head>
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+
+  <header class="main-header">
+    <nav class="navbar navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="index.php" class="navbar-brand"><b><img src="https://upload.wikimedia.org/wikipedia/id/c/cc/Logo-ubl.png" width="50px" height="30px" style="margin-top:-10px;"/> INOVASI UBL 2019</b></a>
+        </div>
+
+        <!-- /.navbar-collapse -->
+        <!-- Navbar Right Menu -->
+        <div class="navbar-custom-menu">
+        </div>
+        <!-- /.navbar-custom-menu -->
+      </div>
+      <!-- /.container-fluid -->
+    </nav>
+  </header>
+  
+  <!-- Full Width Column -->
+  <div class="content-wrapper">
+    <div class="container">
+      <!-- Main content -->
+      <section class="content">
+        <div class="callout callout-info">
+          <p>Semangat Inovasi</p>
+<p style="text-align: justify;">Yth. Bpk/Ibu, mohon kiranya dapat membantu kami, dengan melakukan pengisian data luaran Inovasi yg telah Bpk/Ibu miliki. Baik dalam bentuk Buku, Artikel, Jurnal, Produk yang belum atau telah di daftarkan HKI, SNI, ISBN, PIRT, BPOM, atau sertifikasi dari lembaga lainnya. <br />Seluruh data yg terkumpul akan kami unggah ke Laman inovasiindustri.ristekdikti.go.id sebagai database utk kepentingan kita bersama baik individu maupun lembaga. <br />Sebelum melakukan pengisian, mhn kiranya menyiapkan foto sertifikat/lembar perijinan/cover bagian belakang buku (barcode ISBN) sebagai bahan untuk mengisi yang kemudian di unggah dg format jpg/jpeg/png.<br />Bila Bpk/Ibu memiliki lebih dr 1 (satu) inovasi, Bpk/Ibu dpt menggunakan link yang sama utk pengisian data berikutnya. Terima kasih.</p>
+        </div>
+        <!-- /.box -->
+	
+	<div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Quick Example</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+                  
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Nama Inventor</label>
+                  <textarea class="form-control" rows="3" placeholder="Nama Inventor"></textarea>
+                </div>
+                
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Judul Inventor</label>
+                  <textarea class="form-control" rows="3" placeholder="Judul Inventor"></textarea>
+                </div>
+                
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Kata Kunci</label>
+                  <textarea class="form-control" rows="3" placeholder="Kata Kunci"></textarea>
+                </div>
+                
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Bidang Fokus</label>
+                  <div class="radio icheck-primary">
+                        <input type="radio" checked id="primary1" name="primary" />
+                        <label for="primary1">TIK</label>
+                    </div>
+                    <div class="radio icheck-primary">
+                        <input type="radio" id="primary2" name="primary" />
+                        <label for="primary2">Hankam</label>
+                  </div>
+                  <div class="radio icheck-primary">
+                        <input type="radio" id="primary3" name="primary" />
+                        <label for="primary3">Bahan Baku</label>
+                  </div>
+                  <div class="radio icheck-primary">
+                        <input type="radio" id="primary4" name="primary" />
+                        <label for="primary4">Transportasi</label>
+                  </div>
+                  <div class="radio icheck-primary">
+                        <input type="radio" id="primary5" name="primary" />
+                        <label for="primary5">Pangan</label>
+                  </div>
+                  <div class="radio icheck-primary">
+                        <input type="radio" id="primary6" name="primary" />
+                        <label for="primary6">Kesehatan dan Obat</label>
+                  </div>
+                  <div class="radio icheck-primary">
+                        <input type="radio" id="primary7" name="primary" />
+                        <label for="primary7">Material Maju</label>
+                  </div>
+                  <div class="radio icheck-primary">
+                        <input type="radio" id="primary8" name="primary" />
+                        <label for="primary8">Energi</label>
+                  </div>
+                </div>
+                
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Deskripsi Produk</label>
+                  <textarea class="form-control" rows="3" placeholder="Deskripsi Produk"></textarea>
+                </div>
+                
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Keunggulan Produk</label>
+                  <textarea class="form-control" rows="3" placeholder="Keunggulan Produk"></textarea>
+                </div>
+                
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Bidang Fokus</label>
+                  
+                  <div class="radio icheck-primary">
+                            <input type="radio" id="success1" name="success" />
+                            <label for="success1">< Rp.500.000</label>
+                    </div>
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success2" name="success" />
+                            <label for="success2">Rp.500.000 - Rp.1.000.000</label>
+                    </div>
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success3" name="success" />
+                            <label for="success3">Rp.1.000.000 - Rp.5.000.000</label>
+                    </div>
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success4" name="success" />
+                            <label for="success4">Rp.5.000.000 - Rp.25.000.000</label>
+                    </div>
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success5" name="success" />
+                            <label for="success5">Rp.25.000.000 - Rp.50.000.000</label>
+                    </div>
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success6" name="success" />
+                            <label for="success6">Rp.50.000.000 - Rp.100.000.000</label>
+                    </div>
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success7" name="success" />
+                            <label for="success7">Rp.100.000.000 - Rp.250.000.000</label>
+                    </div>
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success8" name="success" />
+                            <label for="success8">Rp.250.000.000 - Rp.500.000.000</label>
+                    </div>
+                    
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success9" name="success" />
+                            <label for="success9">Rp.500.000.000 - Rp.1.000.000.000</label>
+                    </div>
+                    
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success10" name="success" />
+                            <label for="success10">Rp.1.000.000.000 - Rp.2.000.000.000</label>
+                    </div>
+                    
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success11" name="success" />
+                            <label for="success11">Rp.2.000.000.000 - Rp.5.000.000.000</label>
+                    </div>
+                    
+                    <div class="radio icheck-primary">
+                            <input type="radio" id="success12" name="success" />
+                            <label for="success12">> Rp.5.000.000.000</label>
+                    </div>
+                    
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Nomor Sertifikasi (SNI/ISBN/Tidak Ada)</label>
+                      <textarea class="form-control" rows="3" placeholder="Nomor Sertifikasi"></textarea>
+                    </div>
+                    
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Nomor HKI (Semua yang termasuk dalam HKI/Lainnya/Tidak Ada)</label>
+                      <textarea class="form-control" rows="3" placeholder="Deskripsi Produk"></textarea>
+                    </div>
+                    
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Nomor Perijinan (Ijin Perdagangan/PIRT/BPOM/atau lainnya/Tidak Ada)</label>
+                      <textarea class="form-control" rows="3" placeholder="Deskripsi Produk"></textarea>
+                    </div>
+                    
+                    <div class="form-group">
+                      <label for="exampleInputPassword1">Foto File (Upload barang bukti berupa Sertifikat/Lembar Pengesahan/Cover Belakang Buku dalam format jpg/jpeg/png maks.200kb)</label>
+                      <textarea class="form-control" rows="3" placeholder="Deskripsi Produk"></textarea>
+                    </div>
+                    
+                </div>
+                
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Submit</button>
+              </div>
+            </form>
+          </div>
+	
+	
+          </div>
+          
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+       
+      </div>
+      <strong></strong>
+    </div>
+    <!-- /.container -->
+  </footer>
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="https://adminlte.io/themes/AdminLTE/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="https://adminlte.io/themes/AdminLTE/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="https://adminlte.io/themes/AdminLTE/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="https://adminlte.io/themes/AdminLTE/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes 
+<script src="https://adminlte.io/themes/AdminLTE/dist/js/demo.js"></script>-->
+<!-- date-range-picker -->
+<script src="http://data.ppikubl.com/moment.min.js"></script>
+
+</body>
+</html>
